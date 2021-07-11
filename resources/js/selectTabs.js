6747fcdd6a663ff96e3c79d54ef8471c041e61b1
張珈語 <chiayu.chang@udngroup.com.tw>
2021-07-11 23:07:38 +0800

    媒體發展 卡片區塊切版

--- resources/js/selectTabs.js
+++ resources/js/selectTabs.js
@@ -1,6 +1,11 @@
-function selectTabs(event, id) {
-    const tabs = document.querySelectorAll(id + " .tab");
-    const articles = document.querySelectorAll(id + " .secend-category-article");
+function selectTabs(event, id, level) { //傳入事件, 文章id, 分類
+    if(level == 2) {
+        var tabs = document.querySelectorAll(id + " .tabs-2th .tab");
+        var articles = document.querySelectorAll(id + " .secend-category-article");
+    } else if(level == 3) {
+        var tabs = document.querySelectorAll(id + " .tabs-3th .tab");
+        var articles = document.querySelectorAll(id + " .third-category-article");
+    }
     const articleId = event.target.dataset.id;
     tabs.forEach(tab => {
         tab.classList.remove("selected");
