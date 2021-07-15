5718bcf5d60d96cafd4b558fe7652c9d5a145796
minnan0328 <39487858+minnan0328@users.noreply.github.com>
2021-07-15 17:38:19 +0800

    新增 tabs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
-new Vue({
+let app = new Vue({
     el: "#app",
     data: {
         text: '測試區域',
         items: ['測試1', '測試2', '測試3', '測試4'],
-        sectionTypes: {}
+        sectionTypes: {},
+        tabs: {},
+        TH: 0
     },
     created() {
         axios.get("./../../resources/js/type.json")
@@ -12,9 +14,32 @@ new Vue({
                   let result = response.data.result ;
                   console.log(result);
                   this.sectionTypes = result;
+                  filledTab(this.sectionTypes,this.TH + 1);
+
               } else {
                   console.log('err')
               }
           }).catch(err => console.log(err))
     },
+    mounted(){
+        console.log(this.tabs);
+    }
 });
+
+function filledTab(taxonomies,TH) {
+    taxonomies.childTaxonomies.forEach((a,AIdx) => {
+        if(a.childTaxonomies.length != 0) {
+
+            a.childTaxonomies.forEach((b,BIdx) => {
+
+                if(BIdx == 0) {
+                    app.tabs[a.taxonomyId + 'TH' + TH] = b.taxonomyId;
+                }
+                
+            })
+
+            filledTab(a,TH + 1);
+                
+        } else { return }
+    });
+}
