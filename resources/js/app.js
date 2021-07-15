f6907138d6cdc3480229ef02413b5ee1e852fefd
張珈語 <chiayu.chang@udngroup.com.tw>
2021-07-15 13:30:08 +0800

    測試

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
+vue.component('test', {
+    templat:'../article/template.html',
+})
 new Vue({
     el: "#app",
+    component: {test},
     data: {
         text: '測試區域',
         items: ['測試1', '測試2', '測試3', '測試4']
