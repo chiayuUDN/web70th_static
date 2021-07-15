d2603207242e631509b90e4f8bb3260685e35161
minnan0328 <39487858+minnan0328@users.noreply.github.com>
2021-07-15 21:25:16 +0800

    調整整體 css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,6 @@
 let app = new Vue({
     el: "#app",
     data: {
-        text: '測試區域',
-        items: ['測試1', '測試2', '測試3', '測試4'],
         sectionTypes: {},
         tabs: {},
         order: 0
