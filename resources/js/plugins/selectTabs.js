238737ee312c01e2ae3a116cf4885a597bbaa412
張珈語 <chiayu.chang@udngroup.com.tw>
2021-07-15 17:56:21 +0800

    1.實作第四分類樣式 2.調整架構

--- resources/js/plugins/selectTabs.js
+++ resources/js/plugins/selectTabs.js
@@ -1,10 +1,13 @@
 function selectTabs(event, id, level) { //傳入事件, 文章id, 分類
     if(level == 2) {
-        var tabs = document.querySelectorAll(id + " .tabs-2th .tab");
-        var articles = document.querySelectorAll(id + " .secend-category-article");
+        var tabs = document.querySelectorAll("#"+id + " .tabs-2th .tab");
+        var articles = document.querySelectorAll("#"+id + " .secend-category-article");
     } else if(level == 3) {
-        var tabs = document.querySelectorAll(id + " .tabs-3th .tab");
-        var articles = document.querySelectorAll(id + " .third-category-article");
+        var tabs = document.querySelectorAll("#"+id + " .tabs-3th .tab");
+        var articles = document.querySelectorAll("#"+id + " .third-category-article");
+    } else if(level == 4) {
+        var tabs = document.querySelectorAll("#"+id + " .tabs-4th .tab");
+        var articles = document.querySelectorAll("#"+id + " .fourth-category-article");
     }
     const articleId = event.target.dataset.id;
     tabs.forEach(tab => {
