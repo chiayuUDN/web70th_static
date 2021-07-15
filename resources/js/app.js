6b79a695f8bf3f527f057e5633bec2d959f91912
minnan0328 <39487858+minnan0328@users.noreply.github.com>
2021-07-15 14:24:14 +0800

    add get json

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,21 @@
-
 Vue.component("component-dropdown", {
     template: '#component-dropdown',
 });
-
 
 new Vue({
     el: "#app",
     data: {
         text: '測試區域',
-        items: ['測試1', '測試2', '測試3', '測試4']
-    }
+        items: ['測試1', '測試2', '測試3', '測試4'],
+        sectionTypes: {}
+    },
+    created() {
+
+        axios.get("./../../resources/js/type.json")
+          .then(response => {
+              let result = response.data;
+              console.log(result);
+              this.sectionTypes = result;
+          }).catch(err => console.log(err))
+    },
 });
