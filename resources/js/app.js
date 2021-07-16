77d5a2f55c05a54e3246749cbcd96fd318cce8c8
minnan0328 <39487858+minnan0328@users.noreply.github.com>
2021-07-16 16:33:33 +0800

    暫存

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,29 +2,40 @@ let app = new Vue({
     el: "#app",
     data: {
         sectionTypes: {},
+        currentNav: null,
+        specialBlock: 'mediaFeature',
         tabs: {}, // 紀錄選擇的tabs
-        order: 1, 
+        order: 1,
     },
     created() {
         w3.includeHTML();
-        axios.get("./../../resources/js/type.json")
-          .then(response => {
-              if(response.data.success) {
-                  let result = response.data.result;
-                  result.childTaxonomies = result.childTaxonomies.slice(1)
-                  this.sectionTypes = result;
-                  filledTab(this.sectionTypes,this.order);
-
-              } else {
-                  console.log('err')
-              }
-          }).catch(err => console.log(err))
-    },
-    mounted(){
-        console.log(this.tabs);
+        getSectionTypes().then(response => {
+            if(response.data.success) {
+                let result = response.data.result;
+                this.sectionTypes = result;
+                filledTab(this.sectionTypes,this.order);
+            } else {
+                console.log('err')
+            }
+        }).catch(err => console.log(err));
+        
     },
     updated(){
         w3.includeHTML();
+    },
+    computed:{
+        navigations() {
+            let nav = [];
+            if(Object.keys(this.sectionTypes).length != 0) {
+                this.sectionTypes.childTaxonomies.forEach(item => {
+                    nav.push({
+                        taxonomyName: item.taxonomyName,
+                        taxonomyId: item.taxonomyId
+                    })
+                })
+            }
+            return nav;
+        }
     },
     methods: {
         selectedTab(parentsIdKey,childrenId){
@@ -33,9 +44,15 @@ let app = new Vue({
         },
         isCurrentTab(parentsIdKey,childrenId){
             return this.tabs[parentsIdKey] == childrenId;
-        }
+        },
+        
     }
 });
+
+
+function getSectionTypes() {
+    return  axios.get("./../../resources/js/type.json");
+}
 
 // 傳入分類與層數，來記錄整個資料的第一個分類ID
 function filledTab(taxonomies, order) {
@@ -45,7 +62,6 @@ function filledTab(taxonomies, order) {
                 if(childIdx == 0) {
                     app.tabs[`${parent.taxonomyId}${order}TH`] = child.taxonomyId;
                 }
-                
             })
 
             filledTab(parent, order + 1);
